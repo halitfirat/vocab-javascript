1788d1f061e9368fb775262b5ec60a94eee40d58
halitfirat <fhalitt@gmail.com>
2020-08-05 11:26:32 +0200

    Added client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Vocabs</h1>
+      Learning vocabularies with ease
+    </div>
+  );
+};
+
+export default Landing;
